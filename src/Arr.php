9b5acf7c19f5c99dd26faea84c72c72c6047d4fb
warpcode <git@warpcode.eu>
2017-04-19 20:31:53 +0100

    Add method to check if array in indexed

--- src/Arr.php
+++ src/Arr.php
@@ -55,6 +55,41 @@ class Arr implements /*\IteratorAggregate , \ArrayAccess , */ \Serializable , \C
     }
 
     /**
+     * Checks wither the array has integer keys
+     * @param  boolean $sequential_keys Make sure the keys are sequenctional
+     * @return boolean
+     */
+    public function isIndexed($sequential_keys = true){
+        $previous_index = NULL;
+        $is_index = true;
+        foreach($this->store as $key => &$value){
+            if(!is_int($key)){
+                $is_index = false;
+                break;
+            }
+            elseif($key < 0){
+                // keys less than 0 are not valid indexs for an indexed array
+                $is_index = false;
+                break;
+            }elseif($sequential_keys && $previous_index === null && $key !== 0){
+                //if the first key is not 0, we do not have sequentical keys
+                $is_index = false;
+                break;
+            }
+            elseif($sequential_keys && ($previous_index + 1) !== $key){
+                // current key is not +1 of previous index and therefor breaks sequence
+                $is_index = false;
+                break;
+            }
+
+            $previous_index = $key;
+        }
+        unset($value);
+
+        return $is_index;
+    }
+
+    /**
      * Slices the array into a smaller specified section
      * @param  int $index  Index position to start the slice
      * @param  int $length How many items in the array to retrieve from the specified index
